53a8b8573fd228f61006bbfbb61f2166a3185931
Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
2026-03-06 11:21:55 +0530

    conditionally include chef in gemfile (#90)
    * conditionally include chef
    
    skip this declaration when GEMFILE_MOD is set during external ci tests (in chef repo inside chef/tasks/bin/run_external_test) because GEMFILE_MOD already injects a chef from source and bundler treats that as >= 0 which conflicts with >= 18.0
    
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
    
    * Why is this failing?
    
    Signed-off-by: John McCrae <john.mccrae@progress.com>
    
    * Why is this failing?
    
    Signed-off-by: John McCrae <john.mccrae@progress.com>
    
    ---------
    
    Signed-off-by: Rishi Kumar Chawda <rishichawda@users.noreply.github.com>
    Signed-off-by: John McCrae <john.mccrae@progress.com>
    Co-authored-by: John McCrae <john.mccrae@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -79,6 +79,10 @@ function Invoke-Install {
 }
 
 function Invoke-After {
+    Write-Host "******************************************************************"
+    Write-Host "** What is My Project Root as determined by pkg_prefix? $pkg_prefix"
+    Write-Host "******************************************************************"
+    Sleep 20s
     # We don't need the cache of downloaded .gem files ...
     Remove-Item $pkg_prefix/vendor/cache -Recurse -Force
     # We don't need the gem docs.
