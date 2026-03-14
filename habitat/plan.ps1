c0dd508da191b3dd7d0487b827aacfbf0a368b47
nikhil2611 <ngupta@progress.com>
2026-03-14 23:14:25 +0530

    using bundler 2.3.3
    Signed-off-by: nikhil2611 <ngupta@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -100,6 +100,11 @@ function Invoke-Install {
 }
 
 function Invoke-After {
+    function Invoke-After {
+    Write-Host "******************************************************************"
+    Write-Host "** What is My Project Root as determined by pkg_prefix? $pkg_prefix"
+    Write-Host "******************************************************************"
+    Sleep 20s
     # We don't need the cache of downloaded .gem files ...
     Remove-Item $pkg_prefix/vendor/cache -Recurse -Force
     # We don't need the gem docs.
