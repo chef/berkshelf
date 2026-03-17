92915176a1d3f34564dfefdb5beb4493df7dc8af
nikhil2611 <ngupta@progress.com>
2026-03-17 15:25:59 +0530

    fixing path for windows hab build
    Signed-off-by: nikhil2611 <ngupta@progress.com>

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -81,7 +81,7 @@ function Invoke-Install {
     & $ridkPath enable
 
     write-output "*** invoke-install"
-    $NoticeFile = "$PLAN_CONTEXT\..\..\NOTICE"
+    $NoticeFile = "$PLAN_CONTEXT\..\NOTICE"
 
     if (Test-Path $NoticeFile) {
         Write-BuildLine "** Copying NOTICE to package directory"
